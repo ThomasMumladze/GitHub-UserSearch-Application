cdadc9806293275f83b8651f2581d85bb10c03fe
ThomasMumladze <mumladzea4@gmail.com>
2023-03-14 17:57:40 +0400

    modified

---
+++ src/components/DevFinder.tsx
@@ -0,0 +1,23 @@
+import "./devFinder.scss";
+import Search from "./searchComponent/Search";
+import Title from "./title/Title";
+const moonIcon = require("../assets/icon/Path.png");
+
+const DevFinder = () => {
+    return (
+        <div className="devFinder">
+            <div className={"pageTitle_DarkTheme"}>
+                <Title text={"devfinder"} />
+                <div className="darkModeButton">
+                    <Title text={"dark"} />
+                    <button>
+                        <img src={moonIcon} alt="" />
+                    </button>
+                </div>
+            </div>
+            <Search />
+        </div>
+    );
+};
+
+export default DevFinder;
