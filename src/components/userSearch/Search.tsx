16a1c4419c92cec379e852fffaa3a67639c2c05e
ThomasMumladze <a.mumladze@sda.gov.ge>
2023-03-23 17:26:42 +0400

    search component with styles & functionality

--- src/components/userSearch/Search.tsx
+++ src/components/userSearch/Search.tsx
@@ -1,6 +1,12 @@
 import "./search.scss";
+import { useState } from 'react'
+interface Props {
+    handleGetUserData: (value: string) => void,
+}
+const Search = (props: Props) => {
+    const { handleGetUserData } = props
+    const [search, setSearch] = useState('')
 
-const Search = () => {
     return (
         <div className="searchWrapper">
             <div className="searchInput">
@@ -15,11 +21,13 @@ const Search = () => {
                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                 </svg>
                 <input
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
                     type={"text"}
                     placeholder={"Search GitHub username..."}
                 />
             </div>
-            <button>Search</button>
+            <button onClick={() => handleGetUserData(search)}>Search</button>
         </div>
     );
 };
